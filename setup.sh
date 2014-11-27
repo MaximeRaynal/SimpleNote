64a811acf554f912fb20ea64b1c21903829e974a
Maxime Raynal <me@maxproject.fr>
2014-11-27 20:50:24 +0100

    Mise en place de l'environnement de Dev

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ce fichier est lancé à l'éxécution de la machine
+# Il lance les services requis et donne un accès
+
+cd /var/www
+
+IP=$(ip addr | grep 172 | awk '{ print $2 }' | cut --delimiter=/ -f1)
+echo "La machine est accessible sur http://$IP"
+bash
