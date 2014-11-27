66adacbe0fff6d6aeccfb1957d9380e9390b96c9
Maxime Raynal <me@maxproject.fr>
2014-11-27 22:27:06 +0100

    Mise en place de l'architecture Django + modif VM

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Ce fichier est lancé à l'éxécution de la machine
 # Il lance les services requis et donne un accès
 
-cd /var/www
+cd /var/SimpleNote
 
 IP=$(ip addr | grep 172 | awk '{ print $2 }' | cut --delimiter=/ -f1)
 echo "La machine est accessible sur http://$IP"
