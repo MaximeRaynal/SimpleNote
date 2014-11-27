8d0646bf5e2448b7fbaad8b92266663a6fd0d248
Maxime Raynal <me@maxproject.fr>
2014-11-27 23:43:18 +0100

    Mise en place des models et des premières vu

--- src/Note/models.py
+++ src/Note/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    """ Représentation d'une note comme un ensemble de page """
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    privacy_state = models.CharField(max_length=20)
+    creation_date = models.DateTimeField()
+    last_update = models.DateTimeField()
+    is_crypted = models.BooleanField(default=False)
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.name
+
+    def laodPage(self):
+        """ Charge les pages (fichier stocké) associé à la note """
+        return ''
+
+
+class Tag(models.Model):
+    """ Représentation d'un tag """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    parent = models.ForeignKey('self')
+
+    def __str__(self):
+        return '#' + self.name
--- src/Note/views.py
+++ src/Note/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from Note.models import Note
+from Note.models import Tag
 
-# Create your views here.
+import json
+
+
+def index(request):
+    """ Point d'entrée de l'app appelle Angular JS """
+    return HttpResponse("")
+
+def notes(request):
+    """ Retourne toutes les notes au format JSON """
+    return HttpResponse(json.dumps(Note.objects.all()), content_type='application/json')
+
+def tags(request):
+    """ Retourne  tous les tags au format JSON """
+    return HttpResponse("",content_type='application/json')
