a98b5eaacf4aadd9fce4bd92aee0f1d4964764f3
Maxime Raynal <me@maxproject.fr>
2014-11-30 02:28:17 +0100

    Ajout de filtre sur les méthodes de requêtes

--- src/Note/views.py
+++ src/Note/views.py
@@ -4,15 +4,17 @@ from Note.models import Tag
 
 import json
 
-
+@require_http_methods(["GET"])
 def index(request):
     """ Point d'entrée de l'app appelle Angular JS """
     return HttpResponse("")
 
+@require_http_methods(["GET"])
 def notes(request):
     """ Retourne toutes les notes au format JSON """
     return HttpResponse(json.dumps(Note.objects.all()), content_type='application/json')
 
+@require_http_methods(["GET"])
 def tags(request):
     """ Retourne  tous les tags au format JSON """
     return HttpResponse("",content_type='application/json')
