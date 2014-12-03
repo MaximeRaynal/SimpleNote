992edb9ec2184f3029f1d964d6079dc28876d8ff
Maxime Raynal <me@maxproject.fr>
2014-12-03 16:54:28 +0100

    Test unitaire pour l'extraction des Tags

--- src/Note/tests.py
+++ src/Note/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from note.models import Page
 
 # Create your tests here.
+
+class PageMethodTests(TestCase):
+
+    def test_extract_tags(self):
+        """ Test la méthode d'extraction de tag """
+        p = Page()
+        p.text = """#test
+        Un test #plus long
+        Test un #tag.compose
+        Piege pastag#tag
+        et pour finir #tag1#tag2
+        """
+
+        self.assertSetEqual({'test', 'plus', 'tag.compose',
+                             'tag', 'tag1', 'tag2'}, p.get_tags())
