3d8e3008a2d6bb87d5ae93b4f2481962a62c56e1
Maxime Raynal <me@maxproject.fr>
2014-12-03 23:01:47 +0100

    Mise de place de nouvelle vue / urls
    Implémentation de la vue pour récupérer tout les tags.
    Mise en place de la vue pour récupérer les notes ayants un certains tags

--- src/note/urls.py
+++ src/note/urls.py
@@ -4,5 +4,8 @@ from note import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'notes\.json', views.)
+    url(r'notes\.json', views.notes, name='all_notes'),
+    url(r'tags\.json', views.tags, name='all_tags'),
+    url(r'notes/filter/tags', views.tags, name='notes_filter_by_tag')
+
 )
--- src/note/views.py
+++ src/note/views.py
@@ -12,9 +12,23 @@ def index(request):
 @require_http_methods(["GET"])
 def notes(request):
     """ Retourne toutes les notes au format JSON """
-    return HttpResponse(json.dumps(Note.objects.all()), content_type='application/json')
+    return HttpResponse(json.dumps(Note.objects.all()),
+                                             content_type='application/json')
 
 @require_http_methods(["GET"])
 def tags(request):
-    """ Retourne  tous les tags au format JSON """
+    """ Retourne tous les tags au format JSON
+
+        Le traitement (agrégation + unicité) et entièrement fait en python,
+        c'est probablement plus performant qu'avec SQLite
+    """
+    tags_list = set()
+    tags_list +=
+          [note_tags for note in Note.objects.all() for note_tags in note.tags]
+    return HttpResponse(json.dumps(tags_list),content_type='application/json')
+
+@require_http_methods(["POST"])
+def note_filter_by_tags(request):
+    """ Retourne toutes les notes ayants les tags spécifiés """
+    ##gNote.objects.filter(ta)
     return HttpResponse("",content_type='application/json')
