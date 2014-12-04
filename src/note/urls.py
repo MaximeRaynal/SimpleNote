6dadaf347db74243429a7bce558ff42e259d17cc
Maxime Raynal <me@maxproject.fr>
2014-12-04 22:50:34 +0100

    Mise en conformité des vue + nouvelles urls
    Test fonctionnel pour l'authentification,
    Test fonctionnel des vues :
        - index
        - all_notes (sans datas)
        - note_by_id (retourne 404 pq pas de datas)
        - all_tags (sans datas)
        - note_filter_by_tag (sans data)
    Mise en place de la vue : save_note (uniquement docstring)

--- src/note/urls.py
+++ src/note/urls.py
@@ -5,7 +5,11 @@ from note import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'notes\.json', views.notes, name='all_notes'),
+    url(r'note/id-(?P<note_id>\d+)\.json',
+                                         views.note_by_id, name='note_by_id'),
     url(r'tags\.json', views.tags, name='all_tags'),
-    url(r'notes/filter/tags', views.tags, name='notes_filter_by_tag')
+    url(r'notes/filter/tags', views.notes_filter_by_tags,
+                                                 name='notes_filter_by_tag'),
+    url(r'note/save', views.save_note, name='save_note')
 
 )
--- src/note/views.py
+++ src/note/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
 from note.models import Note
 from note.models import Tag
 
 import json
 
 @require_http_methods(["GET"])
+@login_required
 def index(request):
     """ Point d'entrée de l'app appelle Angular JS """
-    return HttpResponse("")
+    return HttpResponse("Done")
 
 @require_http_methods(["GET"])
+@login_required
 def notes(request):
     """ Retourne toutes les notes au format JSON """
-    return HttpResponse(json.dumps(Note.objects.all()),
-                                             content_type='application/json')
+    return HttpResponse(json.dumps(
+                    list(Note.objects.filter(author=request.user).values())),
+                        content_type='application/json')
 
 @require_http_methods(["GET"])
+@login_required
+def note_by_id(request, note_id):
+    """ Retourne la note ayant l'id passé en paramètre """
+    note = get_object_or_404(Note, pk=note_id)
+    return HttpResponse(json.dumps(note.__dict__),
+                        content_type='application/json')
+
+@require_http_methods(["GET"])
+@login_required
 def tags(request):
     """ Retourne tous les tags au format JSON
 
@@ -23,12 +39,31 @@ def tags(request):
         c'est probablement plus performant qu'avec SQLite
     """
     tags_list = set()
-    tags_list +=
-          [note_tags for note in Note.objects.all() for note_tags in note.tags]
+    tags_list = [note_tags for note in Note.objects.filter(author=request.user)
+                           for note_tags in note.tags]
     return HttpResponse(json.dumps(tags_list),content_type='application/json')
 
 @require_http_methods(["POST"])
-def note_filter_by_tags(request):
+@login_required
+def notes_filter_by_tags(request):
     """ Retourne toutes les notes ayants les tags spécifiés """
-    ##gNote.objects.filter(ta)
+    tags_list = json.load(request.POST['tags_list'])
+    note_list = Note.objects.filter(tags__name__in=tags_list).values()
     return HttpResponse("",content_type='application/json')
+
+
+@require_http_methods(["POST"])
+@login_required
+def save_note(request):
+    """ Fonction de serialization / sauvegarde d'une note
+
+        Cette fonction est appellé automatiquement, régulière par la WebApp
+        Elle doit :
+            - Regarder si la note existe déjà, sinon la créer
+            - Sauvegarder les modifications dans les fichiers correspondants
+            - Mettre à jour la base de donnée pour faciliter les recherches
+            - Renvoyer un code HTTP correspondant
+    """
+    pass
+
+
