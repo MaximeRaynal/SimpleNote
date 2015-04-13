28d83f3121e0583bafdc846f24ed783b0b89a06f
Maxime Raynal <maxraynal@gmail.com>
2015-04-13 12:34:49 +0200

    Suppression d'un import inutile

--- src/note/views.py
+++ src/note/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
