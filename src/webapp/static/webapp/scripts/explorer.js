d684c3a605c38bde10ee0ce62742373fea5336be
Maxime Raynal <maxraynal@gmail.com>
2015-04-13 23:36:33 +0200

    Avancement de l'UI

--- src/webapp/static/webapp/scripts/explorer.js
+++ src/webapp/static/webapp/scripts/explorer.js
@@ -4,6 +4,9 @@ $( document ).ready(function() {
 
     loadAllTags();
 
+    loadAllNote();
+
+
 });
 
 function affectEvent() {
@@ -21,8 +24,52 @@ function loadAllTags() {
     }).done(function (data) {
         $("#tags-list").empty();
 
-        data.tagList.forEach(function (elemnt, index, array) {
-            $("#tags-list").append('<button type="button" class="btn btn-default tag">' + elemnt + '</button>');
+        data.forEach(function (elemnt, index, array) {
+            $("#tags-list").append(
+                '<button type="button" class="btn btn-default tag">' + elemnt +
+                '</button>');
+            $(".tag:contains(" + elemnt + ")").click(function () {
+                $( this ).toggleClass("active");
+            });
+        });
+
+    });
+}
+
+function loadAllNote() {
+    $.ajax({
+        url: "/api/notes.json",
+        method: "GET",
+        dataType: "json"
+
+    }).done(function (data) {
+        $("#notes-list").empty();
+
+        data.forEach(function (elemnt, index, array) {
+            $("#notes-list").append(
+                '<div class="col-md-2">  \
+                        <div class="panel panel-default note"> \
+                            <div class="panel-heading">
+                                <h3 class="panel-title">' + elemnt.name + '</h3> \
+                            </div> \
+                            <div class="panel-body"> \
+                                <div class="media"> \
+                                    <div class="media-center media-middle"> \
+                                        <img class="media-object note-pic" src="{% static \'webapp/file.png\' %}" alt="File icons"> \
+                                    </div> \
+                                </div> \
+                                <span class="note-uuid hidden">' + elemnt.uuid + '</span> \
+                            </div> \
+                            <div class="panel-footer clearfix"> \
+                                <button type="button" class="btn btn-default action-button pull-left" > \
+                                    <span class="glyphicon glyphicon-pencil"></span> \
+                                </button> \
+                                <button type="button" class="btn btn-default action-button pull-right" > \
+                                    <span class="glyphicon glyphicon-eye-open"></span> \
+                                </button> \
+                            </div> \
+                        </div> \
+                    </div>');
         });
 
     });
