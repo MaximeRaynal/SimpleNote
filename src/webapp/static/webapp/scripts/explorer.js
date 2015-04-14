a17d8c40471952bcca21eeef6b77f8ab3694b6e5
Maxime Raynal <maxraynal@gmail.com>
2015-04-14 23:09:35 +0200

    Formattage correct de la string

--- src/webapp/static/webapp/scripts/explorer.js
+++ src/webapp/static/webapp/scripts/explorer.js
@@ -47,29 +47,29 @@ function loadAllNote() {
 
         data.forEach(function (elemnt, index, array) {
             $("#notes-list").append(
-                '<div class="col-md-2">  \
-                        <div class="panel panel-default note"> \
-                            <div class="panel-heading">
-                                <h3 class="panel-title">' + elemnt.name + '</h3> \
-                            </div> \
-                            <div class="panel-body"> \
-                                <div class="media"> \
-                                    <div class="media-center media-middle"> \
-                                        <img class="media-object note-pic" src="{% static \'webapp/file.png\' %}" alt="File icons"> \
-                                    </div> \
-                                </div> \
-                                <span class="note-uuid hidden">' + elemnt.uuid + '</span> \
-                            </div> \
-                            <div class="panel-footer clearfix"> \
-                                <button type="button" class="btn btn-default action-button pull-left" > \
-                                    <span class="glyphicon glyphicon-pencil"></span> \
-                                </button> \
-                                <button type="button" class="btn btn-default action-button pull-right" > \
-                                    <span class="glyphicon glyphicon-eye-open"></span> \
-                                </button> \
-                            </div> \
-                        </div> \
-                    </div>');
+                '<div class="col-md-2">' +
+                        '<div class="panel panel-default note"> ' +
+                            '<div class="panel-heading">' +
+                                '<h4 class="panel-title">' + elemnt.name + '</h4> ' +
+                            '</div>' +
+                            '<div class="panel-body">' +
+                                '<div class="media">' +
+                                    '<div class="media-center media-middle">' +
+                                        '<img class="media-object note-pic" src="/static/webapp/file.png" alt="File icons">' +
+                                    '</div>' +
+                                '</div>' +
+                                '<span class="note-uuid hidden">' + elemnt.uuid + '</span>' +
+                            '</div>' +
+                            '<div class="panel-footer clearfix">' +
+                                '<button type="button" class="btn btn-default action-button pull-left" >' +
+                                    '<span class="glyphicon glyphicon-pencil"></span>' +
+                                '</button>' +
+                                '<button type="button" class="btn btn-default action-button pull-right" >' +
+                                    '<span class="glyphicon glyphicon-eye-open"></span>' +
+                                '</button>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>');
         });
 
     });
